e2be093da9a564adcfa713aca3dd49fa8d4f8d68
DANTE-lab212 <alsaadmolham1@gmail.com>
2026-05-24 15:07:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("hello")
+require('dotenv').config();
+
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`${client.user.tag} online`);
+});
+
+client.on('messageCreate', (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === 'hello') {
+    message.reply('hello back 👋');
+  }
+});
+
+client.login(process.env.TOKEN);
